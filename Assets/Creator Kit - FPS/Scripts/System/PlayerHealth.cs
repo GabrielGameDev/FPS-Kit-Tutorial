9d26a742b241ead86760cc9121d747feda339d99
Gabriel-PC\Gabriel <gabrielbarbosa@msn.com>
2019-11-27 10:45:50 -0300

    Dano no jogador

---
+++ Assets/Creator Kit - FPS/Scripts/System/PlayerHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+	public int maxHealth = 100;
+	private int currentHealth;
+
+	public Image damageImage;
+	public Slider healthSlider;
+	public Color damageColor;
+	public float flashSpeed = 2;
+
+	private bool damaged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (damaged)
+		{
+			damageImage.color = damageColor;
+		}
+		else
+		{
+			damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+		}
+		damaged = false;
+    }
+
+	public void TakeDamage(int damageAmount)
+	{
+		currentHealth -= damageAmount;
+		healthSlider.value = currentHealth;
+		damaged = true;
+	}
+}
